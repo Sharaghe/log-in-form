fe53ddcd347e2836bba561a9dd8fa2dfe0b92cf9
Florian Kattner <florian.kattner@gmail.com>
2022-07-29 11:58:21 +0200

    Red border is only added when passwords dont match; Removed from other inputs

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-let submitButton = document.querySelector("button.login");
-const form = document.querySelector("form");
-//form.noValidate = true;
-
-//form.addEventListener('submit', validateForm);
-
 let passwordInput = document.querySelector("input[type='password']#password");
 let passwordConfirmInput = document.querySelector("input[type='password']#confirm-password");
 
@@ -15,31 +9,11 @@ function checkForSamePassword(){
     if (passwordInput.value !== passwordConfirmInput.value){
         passwordConfirmInput.setCustomValidity("false");
         passwordConfirmInput.parentElement.classList.add("invalid");
+        passwordConfirmInput.classList.add("red-border");
     } else {
         passwordConfirmInput.setCustomValidity("");
         passwordConfirmInput.parentElement.classList.remove("invalid");
+        passwordConfirmInput.classList.remove("red-border");
     }
 }
 
-function validateForm(e){
-    let form = e.target;
-
-    if(form.checkValidity()){
-        e.preventDefault();
-
-    } else {
-        e.preventDefault();
-
-        Array.from(form.elements).forEach(i => {
-            if(i.checkValidity()){
-                i.parentElement.classList.remove("invalid");
-                i.classList.remove("invalid");
-                console.log("2");
-            } else {
-                i.parentElement.classList.add("invalid");
-                i.classList.add("invalid");
-            }
-        })
-    }
-
-}
